cd1ef82656b717be61fddc69c38ae0761e4225c5
farrinfedra <56314236+farrinfedra@users.noreply.github.com>
2021-03-28 05:52:37 +0300

    modify

---
+++ Components/HeadData.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from 'react';
+const HeadData = () => {
+    <Head>
+        <title>Farrin Marouf Sofian</title>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="format-detection" content="telephone=no"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="author" content="Farrin"/>
+        <meta name="keywords"
+              content="Farrin Marouf Sofian Portfolio"/>
+        <meta name="description"
+              content="Farrin Marouf Sofian Portfolio"/>
+    </Head>
+}
+export default HeadData;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import MainContent from "../Components/Content/MainContent";
 
 export default function Home() {
 	return (
+
 		<MainContent/>
 	)
 }
